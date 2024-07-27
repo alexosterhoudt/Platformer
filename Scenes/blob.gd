a136e1144cf1cecdd236276e6dce0dc0e2dd1582
alexosterhoudt <alex_osterhoudt94@yahoo.com>
2024-07-27 18:54:49 -0400

    Fixed enemy state machine

--- Scenes/blob.gd
+++ Scenes/blob.gd
@@ -1,14 +1,73 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var player = $"../Player"
 
 var gravity = 980
 
+enum STATE
+{
+	IDLE,
+	FALL,
+	ATTACK
+}
+
+var currentState : STATE
+var isAttacking = false
+
 func _ready():
-	animated_sprite.play("Idle")
+	_set_state(STATE.IDLE)
 
 func _physics_process(delta):
-	if !is_on_floor():
-		velocity.y += gravity * delta
-	move_and_slide()
+	_update_state(delta)
+
+func _set_state(new_state: STATE):
+	if(currentState == new_state):
+		return
+	_exit_state()
+	currentState = new_state
+	_enter_state()
+
+func _enter_state():
+	match currentState:
+		STATE.IDLE:
+			animated_sprite.play("Idle")
+		STATE.FALL:
+			pass
+		STATE.ATTACK:
+			isAttacking = true
+			animated_sprite.play("Attack")
+
+func _update_state(delta: float):
+	match currentState:
+		STATE.IDLE:
+			if (!is_on_floor()):
+				_set_state(STATE.FALL)
+			elif(Input.is_action_just_pressed("attack")):
+				_set_state(STATE.ATTACK)
+				isAttacking = true
+		STATE.FALL:
+			if is_on_floor(): # If the ground is reached, change back to idle
+				_set_state(STATE.IDLE)
+			else: # if still in the air, apply gravity
+				velocity.y += gravity * delta
+			if(Input.is_action_just_pressed("attack")):
+				_set_state(STATE.ATTACK)
+				isAttacking = true
+			move_and_slide()
+		STATE.ATTACK:
+			velocity.x = 0
+			if !is_on_floor():
+				velocity.y += gravity * delta
+			elif is_on_floor() && !isAttacking:
+				_set_state(STATE.IDLE)
+			
+			move_and_slide()
+
+func _exit_state() -> void:
+	match currentState:
+		STATE.IDLE: # Exit IDLE state logic
+			pass
+		STATE.FALL:
+			pass
+		STATE.ATTACK:
+			isAttacking = false
